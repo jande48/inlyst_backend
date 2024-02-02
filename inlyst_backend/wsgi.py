1494511d6c611111663ced6e14dab0aebb405c88
Jacob Anderson <jacob@thepaperlessagent.com>
2024-02-02 16:23:08 -0600

    added better setting and uwsgi files

--- inlyst_backend/wsgi.py
+++ inlyst_backend/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+# # add the hellodjango project path into the sys.path
+sys.path.append("/opt/inlyst/project/inlyst_backend")
 
+# # add the virtualenv site-packages path to the sys.path
+sys.path.append("/opt/inlyst/venv/Lib/site-packages")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inlyst_backend.settings')
 
 application = get_wsgi_application()
