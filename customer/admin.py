901fc80108b4ab47fb971a49ce624e8cfe6b8a2d
Jacob Anderson <jacob@thepaperlessagent.com>
2024-06-01 14:37:32 -0500

    cleaned up the admin

--- customer/admin.py
+++ customer/admin.py
@@ -3,5 +3,9 @@ from customer.models import Customer, Employee, Credentials, Device
 
 admin.site.register(Customer)
 admin.site.register(Employee)
-admin.site.register(Credentials)
 admin.site.register(Device)
+
+
+@admin.register(Credentials)
+class CredentialsAdmin(admin.ModelAdmin):
+    list_display = ("name", "api_key")
